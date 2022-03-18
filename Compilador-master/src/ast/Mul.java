045f5f093a0e83a61c72b9d0045638378ea873f5
Oscar C <56674419+OscarCal@users.noreply.github.com>
2022-03-18 01:10:35 +0100

    Delete Compilador-master directory

--- Compilador-master/src/ast/Mul.java
+++
@@ -1,9 +0,0 @@
-package ast;
-
-public class Mul extends EBin {
-   public Mul(E opnd1, E opnd2) {
-     super(opnd1,opnd2);  
-   }     
-   public KindE kind() {return KindE.MUL;}
-   public String toString() {return "mul("+opnd1().toString()+","+opnd2().toString()+")";}
-}
