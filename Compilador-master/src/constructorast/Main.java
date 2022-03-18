58f77a34a2f94e4032e7eb78338c77cc81eb14a8
Oscar C <56674419+OscarCal@users.noreply.github.com>
2022-03-18 01:02:52 +0100

    Gramatica v1 funcional con cambios en lenguaje

---
+++ Compilador-master/src/constructorast/Main.java
@@ -0,0 +1,31 @@
+package constructorast;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import alex.AnalizadorLexicoExp;
+
+import alex.UnidadLexica;
+
+public class Main {
+   public static void main(String[] args) throws Exception {
+     Reader input = new InputStreamReader(new FileInputStream(args[0]));
+	 //AnalizadorLexicoExp alex = new AnalizadorLexicoExp(input);
+	 //ConstructorASTExp constructorast = new ConstructorASTExp(alex);
+	 //System.out.println(constructorast.lexema());
+	 /*
+	 UnidadLexica unidad;
+	 do {
+		 unidad = (UnidadLexica) alex.next_token();
+		 System.out.println(unidad);
+	 }while(unidad.clase() != ClaseLexica.EOF);
+	 */
+	 
+	 
+     AnalizadorLexicoExp alex = new AnalizadorLexicoExp(input);
+	 AnalizadorSintactico asint = new AnalizadorSintactico(alex);
+	 asint.parse();
+	 
+ }
+}   
+   
